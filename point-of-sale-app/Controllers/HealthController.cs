b6b85b0b9859a422ea835a33453964be179c2a07
nick-rajwade <nikhil.rajwade@colorado.edu>
2023-10-04 15:41:26 -0400

    more instrumentation

--- point-of-sale-app/Controllers/HealthController.cs
+++ point-of-sale-app/Controllers/HealthController.cs
@@ -18,5 +18,14 @@ namespace point_of_sale_app.Controllers
         {
             return "pong";
         }
+
+        //implement a POST method to receive a JSON payload
+        //and return a JSON response
+        [HttpPost(Name = "/ping/{value}")]
+        public string Post([FromBody] string value)
+        {
+            return "pong";
+        }
     }
+
 }
